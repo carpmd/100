d56130c335d3e7c7fc91b984557341221da5758d
Petru Raileanu <raileanu.petru@gmail.com>
2025-08-30 20:28:16 +0300

    02 lesson is ready

---
+++ 2/tip.py
@@ -0,0 +1,5 @@
+print("Welcome to the tip calculator")
+total_bill = int(input("what was the total bill? "))
+tip = int(input("How much tip would you like to give 10, 20 or 15? "))
+people = int(input("How many people to split the bill? "))
+print(f"Each person should pay {(total_bill + total_bill / 100 * tip)/people}")
