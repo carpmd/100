ab1d69f28eeac3bc9331c33edeaab915fff5c5e6
Petru Raileanu <raileanu.petru@gmail.com>
2025-08-30 18:30:57 +0300

    01 lesson is ready

---
+++ band.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's mame?\n")
+print(f"Your band name could be {city} {pet}")
